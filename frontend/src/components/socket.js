4910c822f1f8131ae8a33976f628851db2668232
Michal Czuper <michal.czuper@aalto.fi>
2020-12-07 03:47:17 +0000

    Added polling back in

--- frontend/src/components/socket.js
+++ frontend/src/components/socket.js
@@ -2,6 +2,6 @@ import io from 'socket.io-client'
 
 const ENDPOINT = 'localhost:5000';
 
-let socket = io(ENDPOINT, { transports: ['websocket'] });
+let socket = io(ENDPOINT, { transports: ['polling', 'websocket'] });
 
 export default socket;
